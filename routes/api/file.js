f5b1185b87e6393a98fab1c5cec9a3d851e94ce5
sdaviid <sdaviid@gmail.com>
2022-06-29 22:04:28 -0300

    roles and remove trash

--- routes/api/file.js
+++ routes/api/file.js
@@ -6,20 +6,11 @@ var authCheck = require('../auth');
 var file_controller = require('../../controllers/file');
 
 
-const checkRoles = (roles) => {
-  return (req, res, next) => {
-    if(roles === 'kkkk')
-      console.log('kkkkkkkkkkkkkkkkkkkkk')
-    next();
-  }
-}
-
-
 
 
 router.get('/', authCheck.authenticated, file_controller.index);
 router.post('/create', authCheck.authenticated, file_controller.create);
-router.get('/list', authCheck.authenticated, authCheck.validateRoles(['admin']), file_controller.list);
+router.get('/list', authCheck.authenticated, authCheck.validateRoles(['admin', 'server', 'user']), file_controller.list);
 router.get('/see', authCheck.authenticated, file_controller.findName);
 
 module.exports = router;
